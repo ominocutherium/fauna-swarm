96798eb0530012cb259b6439a4e5375a6050c2cf
Argent <10303636+ominocutherium@users.noreply.github.com>
2022-07-08 10:14:31 -0600

    Apply GNU Affero Public License to all functional code files for sharing

--- game/map/BackgroundTiles.gd
+++ game/map/BackgroundTiles.gd
@@ -1,3 +1,27 @@
+# This file is a part of "Fauna Swarm", a game produced
+# for "RTS Jam 1998" running from June 24th-July 28th on itch:
+# <https://itch.io/jam/rts-jam-1998>
+
+# A copy of the Godot 3.x editor is required to export this game.
+# See <https://godotengine.org/license> for information about how
+# Godot Engine is licensed.
+
+# Gameplay code Copyright (C) 2022 ominocutherium and contributors
+
+# The base game (but not the assets therein, which are licensed separately)
+# is free software: you can redistribute it and/or modify it under the terms
+# of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 extends MapTileData
 
 class_name BackgroundTileData
